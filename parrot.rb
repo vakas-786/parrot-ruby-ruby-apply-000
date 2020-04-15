8058ae74793b1ba5e848a163df0bb3a919c787c0
effective-model-0741 <vakhtar@fordham.edu>
2020-04-15 04:12:31 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "Squawk!")
+def parrot(phrase = "Squawk!", "Pretty bird!")
   puts "#{phrase}"
-  "Pretty bird!"
 end
